cf73d2d80aac6a3805af8f94cc8d4d33469f9f22
daiki.hasegawa <hase.daiki.0517@gmail.com>
2018-10-06 17:22:59 +0000

    bootstrap

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -1,29 +1,29 @@
 @extends('layouts.app')
 
 @section('content')
-
-<h1>メッセージ新規作成ページ</h1>
-
- @if (count($errors) > 0)
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    @endif
-
-{{ Form::model($task, ['route' => 'tasks.store']) }}
-
-{{Form::label('status','ステータス:')}}
-
-{{Form::text('status')}}
-
-{{Form::label('content','メッセージ:')}}
-
-{!! Form::text('content') !!}
-
-{!! Form::submit('投稿') !!}
-
-{!! Form::close() !!}
-
-@endsection
+    
+    <h1>タスク新規作成ページ</h1>
+    
+    
+    <div class="row">
+      <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-lg-offset-3 col-lg-6">
+  
+        {!! Form::model($task, ['route' => 'tasks.store']) !!}
+      
+          <div class="form-group">
+            {!! Form::label('status', 'ステータス:') !!}
+            {!! Form::text('status', null, ['class' => "form-control"]) !!}
+          </div>
+    
+          <div class="form-group">
+            {!! Form::label('content', 'タスク:') !!}
+            {!! Form::text('content', null, ['class' => "form-control"]) !!}
+          </div>
+        
+        {!! Form::submit('投稿', ['class' => "btn btn-primary"]) !!}
+    
+    {!! Form::close() !!}
+      </div>
+    </div>
+    
+@endsection 
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -2,30 +2,29 @@
 
 @section('content')
 
-<h1>id: {{ $task->id }}編集ページ</h1>
-
- @if (count($errors) > 0)
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    @endif
-
-{!! Form::model($task,['route' => ['tasks.update',$task->id], 'method' => 'put']) !!}
-
-{!! Form::label('status','ステータス:') !!}
-
-{!! Form::text('status') !!}
-
-{!! Form::label('content', 'メッセージ:') !!}
-
-{!! Form::text('content') !!}
-
-{!! Form::submit('更新') !!}
-
-{!! Form::close() !!}
-
+    <h1>id: {{ $task->id }}のタスク編集ページ</h1>
+    
+    <div class="row">
+      <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-lg-offset-3 col-lg-6">
+    
+        {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
+    
+          <div class="form-group">
+            {!! Form::label('status', 'ステータス:') !!}
+            {!! Form::text('status', null, ['class' => "form-control"]) !!}
+          </div>
+    
+          <div class="form-group">
+            {!! Form::label('content', 'タスク:') !!}
+            {!! Form::text('content', null, ['class' => "form-control"]) !!}
+          </div>
+        
+        {!! Form::submit('更新', ['class' => "btn <btn-default></btn-default>"]) !!}
+    
+        {!! Form::close() !!}
+    
+      </div>
+    </div>      
 
 
 @endsection
